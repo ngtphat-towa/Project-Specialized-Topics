b7508e0b45d643bdc0dd21d9f79ba05f32ba9241
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2023-11-16 16:39:31 +0700

    dev: implement `Category API`

--- src/backend/src/controllers/category.controller.ts
+++ src/backend/src/controllers/category.controller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { Category, ICategory } from "../models/category.model";
 
 const getCategoriesByFilter = async (
   req: Request,
@@ -6,7 +7,8 @@ const getCategoriesByFilter = async (
   next: NextFunction
 ) => {
   try {
-    return res.status(200).json({ message: "getCategoriesByFilter" });
+    const categories = await Category.find(req.query);
+    return res.status(200).json(categories);
   } catch (error) {
     next(error);
   }
@@ -14,7 +16,11 @@ const getCategoriesByFilter = async (
 
 const getCategory = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    return res.status(200).json({ message: "getCategory" });
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+    return res.status(200).json(category);
   } catch (error) {
     next(error);
   }
@@ -26,7 +32,9 @@ const createCategory = async (
   next: NextFunction
 ) => {
   try {
-    return res.status(200).json({ message: "createCategory" });
+    const category: ICategory = new Category(req.body);
+    const result = await category.save();
+    return res.status(201).json(result);
   } catch (error) {
     next(error);
   }
@@ -38,7 +46,13 @@ const updateCategory = async (
   next: NextFunction
 ) => {
   try {
-    return res.status(200).json({ message: "getAll" });
+    const category = await Category.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!category) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+    return res.status(200).json(category);
   } catch (error) {
     next(error);
   }
---
+++ src/backend/src/models/category.model.ts
@@ -0,0 +1,49 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+// Define the Category schema
+const CategorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  description: {
+    type: String,
+    trim: true,
+    maxlength: 200,
+  },
+  imageUrl: {
+    type: String,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+  },
+});
+
+// Add a pre-save middleware to handle updatedAt
+CategorySchema.pre("save", function (next) {
+  if (this.isModified()) {
+    this.updatedAt = new Date();
+  }
+  next();
+});
+
+// Define the Category model
+export interface ICategory extends Document {
+  name: string;
+  description?: string;
+  imageUrl?: string;
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
+// Create the Category model
+export const Category = mongoose.model<ICategory>("Category", CategorySchema);
