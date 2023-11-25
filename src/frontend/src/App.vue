f4639e38cb00f67a75441f5f416e9c339052c07e
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2023-11-25 11:08:00 +0700

    dev : remove default components

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,14 +1,11 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div>"You did it!"</div>
     </div>
   </header>
 
