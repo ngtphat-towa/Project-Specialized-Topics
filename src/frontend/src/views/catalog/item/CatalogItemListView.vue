3918d9b8d702afcfdbe3a69ec21bab00f5561650
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2023-11-26 17:20:53 +0700

    dev: populate `CatalogItem`

--- src/frontend/src/views/catalog/item/CatalogItemListView.vue
+++ src/frontend/src/views/catalog/item/CatalogItemListView.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="container">
-    <div class="d-flex justify-content-between align-items-center mb-3">
-      <h1>Employee Table</h1>
-      <button class="btn btn-primary" @click="showAddForm = !showAddForm">Add Employee</button>
-    </div>
+    <h1>Employee Table</h1>
     <div class="mb-3 d-flex justify-content-between">
       <input v-model="searchTerm" class="form-control" placeholder="Search..." />
       <select v-model="searchField" class="form-control">
-        <option value="">Select field to search...</option>
-        <option value="name">Name</option>
+        <label for=""></label>
+        <option value="name" selected>Name</option>
         <option value="department">Department</option>
         <option value="age">Age</option>
         <option value="dateOfBirth">Date Of Birth</option>
@@ -59,182 +56,174 @@
     <table class="table table-bordered">
       <thead class="thead-dark">
         <tr>
+          <th scope="col">Image</th>
           <th scope="col">Name</th>
-          <th scope="col">Department</th>
-          <th scope="col">Age</th>
-          <th scope="col">Date Of Birth</th>
+          <th scope="col">Price</th>
+          <th scope="col">SKU</th>
+          <th scope="col">Type</th>
+          <th scope="col">Brand</th>
           <th scope="col">Actions</th>
         </tr>
       </thead>
       <tbody>
-        <template v-for="(item, index) in paginatedItems" :key="item.name">
+        <template v-for="(item, index) in items" :key="item.name">
           <tr>
-            <td>{{ item.name }}</td>
-            <td>{{ item.department }}</td>
-            <td>{{ item.age }}</td>
-            <td>{{ item.dateOfBirth }}</td>
             <td>
-              <button class="btn btn-success" @click="editItem(index)">Edit</button>
-              <button class="btn btn-danger" @click="deleteItem(index)">Delete</button>
-              <button class="btn btn-primary" @click="showDetails(item)">Details</button>
-              <button class="btn btn-danger" @click="confirmDelete(index)">Delete 2</button>
+              <img class="review-image" :src="convertToBase64Image(item)" alt="Image preview" />
+            </td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.price }}</td>
+            <td>{{ item.availableStock }}</td>
+            <td>{{ item.catalogType.name }}</td>
+            <td>{{ item.catalogBrand.name }}</td>
+            <td>
+              <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-primary" @click="showDetails(index)">
+                  Details
+                </button>
+                <button type="button" class="btn btn-success" @click="editItem(index)">
+                  Edit Item
+                </button>
+                <button class="btn btn-danger" @click="confirmDelete(index)">Delete Item</button>
+              </div>
             </td>
           </tr>
+          <tr v-if="showDetailIndex === index">
+            <td colspan="7">{{ item.description }}</td>
+          </tr>
           <tr v-if="editingIndex === index">
-            <td colspan="5">
-              <h2>Edit Employee</h2>
+            <td colspan="7">
+              <h2>Edit Product</h2>
               <form @submit.prevent="updateItem" class="p-3 border rounded">
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label font-weight-bold">Name</label>
-                  <div class="col-sm-10">
-                    <input class="form-control" v-model="items[editingIndex].name" />
-                  </div>
+                <div class="form-group">
+                  <label>Image</label>
+                  <input type="file" class="form-control" @change="onImageChange" required />
+                  <!-- <p v-if="errors.image" class="text-danger">{{ errors.image }}</p> -->
                 </div>
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label font-weight-bold">Department</label>
-                  <div class="col-sm-10">
-                    <input class="form-control" v-model="items[editingIndex].department" />
-                  </div>
+                <div class="form-group" v-if="reviewImage">
+                  <label>Review Image</label>
+                  <img :src="reviewImage" alt="Image preview" style="width: 100%; height: auto" />
                 </div>
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label font-weight-bold">Age</label>
-                  <div class="col-sm-10">
-                    <input class="form-control" v-model="items[editingIndex].age" />
-                  </div>
+                <div class="form-group">
+                  <label>Price</label>
+                  <input class="form-control" v-model="items[editingIndex].price" />
                 </div>
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label font-weight-bold">Date Of Birth</label>
-                  <div class="col-sm-10">
-                    <input class="form-control" v-model="items[editingIndex].dateOfBirth" />
-                  </div>
+                <div class="form-group">
+                  <label>SKU</label>
+                  <input class="form-control" v-model="items[editingIndex].availableStock" />
                 </div>
-                <button class="btn btn-success" type="submit">Save</button>
-                <button class="btn btn-danger" @click="cancelEdit">Cancel</button>
+                <div class="form-group">
+                  <label>Type</label>
+                  <input class="form-control" v-model="items[editingIndex].catalogBrand.name" />
+                </div>
+                <div class="form-group">
+                  <label>Brand</label>
+                  <input class="form-control" v-model="items[editingIndex].catalogType.name" />
+                </div>
+                <div class="form-group">
+                  <label>Description</label>
+                  <textarea
+                    class="form-control"
+                    id="w3review"
+                    name="description"
+                    v-model="items[editingIndex].description"
+                    rows="4"
+                    cols="50"
+                  ></textarea>
+                </div>
+                <button class="btn btn-success" @click="updateItem(item)" type="submit">
+                  Save
+                </button>
+                <button class="btn btn-danger" @click="cancelEdit(item)">Cancel</button>
               </form>
             </td>
           </tr>
         </template>
       </tbody>
     </table>
-    <nav aria-label="Page navigation example">
-      <ul class="pagination">
-        <li class="page-item" :class="{ disabled: currentPage === 1 }">
-          <a class="page-link" href="#" @click.prevent="currentPage--">Previous</a>
-        </li>
-        <li
-          class="page-item"
-          v-for="page in totalPages"
-          :key="page"
-          :class="{ active: page === currentPage }"
-        >
-          <a class="page-link" href="#" @click.prevent="currentPage = page">{{ page }}</a>
-        </li>
-        <li class="page-item" :class="{ disabled: currentPage === totalPages }">
-          <a class="page-link" href="#" @click.prevent="currentPage++">Next</a>
-        </li>
-      </ul>
-    </nav>
   </div>
 </template>
 
 <script>
-import { computed, ref } from 'vue';
-import { tmpData } from './temp';
+import { ref } from 'vue';
+// import { productItems } from './tempProduct.js';
+import convertToBase64 from '../../../services/image/image.render';
+import catlogItemService from '../../../services/catalog/item.service';
+
 export default {
+  name: 'CatalogItemListView',
   setup() {
-    const items = ref(tmpData);
-    const searchTerm = ref('');
-    const searchField = ref('');
-    const sortField = ref('');
-    const sortMode = ref('asc');
+    const items = ref(null);
     const editingIndex = ref(null);
-    const currentPage = ref(1);
-    const itemsPerPage = ref(10);
-    const newEmployee = ref({ name: '', department: '', age: '', dateOfBirth: '' });
+    const showDetailIndex = ref(null);
     const showAddForm = ref(false);
-    // The rest of your setup code goes here...
 
-    const addEmployee = () => {
-      items.value.push({ ...newEmployee.value });
-      newEmployee.value = { name: '', department: '', age: '', dateOfBirth: '' };
+    const showDetails = (index) => {
+      showDetailIndex.value = showDetailIndex.value === index ? null : index;
+    };
+
+    const cancelAdd = () => {
       showAddForm.value = false;
-    };
-
-    const filteredAndSortedItems = computed(() => {
-      let result = items.value;
-      if (searchTerm.value && searchField.value) {
-        result = result.filter((item) =>
-          item[searchField.value].toString().includes(searchTerm.value)
-        );
-      }
-      if (sortField.value) {
-        result = result.sort((a, b) => {
-          if (a[sortField.value] < b[sortField.value]) return sortMode.value === 'asc' ? -1 : 1;
-          if (a[sortField.value] > b[sortField.value]) return sortMode.value === 'asc' ? 1 : -1;
-          return 0;
-        });
-      }
-      return result;
-    });
-
-    const totalPages = computed(() =>
-      Math.ceil(filteredAndSortedItems.value.length / itemsPerPage.value)
-    );
-
-    const paginatedItems = computed(() => {
-      const start = (currentPage.value - 1) * itemsPerPage.value;
-      const end = start + itemsPerPage.value;
-      return filteredAndSortedItems.value.slice(start, end);
-    });
-
-    const showDetails = (item) => {
-      alert(JSON.stringify(item, null, 2));
-    };
-
-    const deleteItem = (index) => {
-      this.items = this.items.splice(index, 1);
-    };
-
-    const  confirmDelete= (index)=> {
-      if (window.confirm('Are you sure you want to delete this employee?')) {
-        this.deleteItem(index);
-      }
     };
 
     const editItem = (index) => {
       editingIndex.value = editingIndex.value === index ? null : index;
     };
+    const cancelEdit = () => {
+      this.editingIndex = null;
+    };
 
     const updateItem = () => {
       editingIndex.value = null;
     };
-
-    const cancelEdit = () => {
-      editingIndex.value = null;
+    const confirmDelete = (index) => {
+      if (window.confirm('Are you sure you want to delete this employee?')) {
+        deleteItem(index);
+      }
     };
 
+    const deleteItem = (index) => {
+      items.value.splice(index, 1);
+    };
+
+    const getCatalogItemData = async () =>
+      await catlogItemService
+        .getAllItems()
+        .then((response) => {
+          console.log(response);
+          const itemsData = response.data.data;
+          items.value = itemsData;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     return {
-      newEmployee,
-      showAddForm,
       items,
-      searchTerm,
-      searchField,
-      sortField,
-      sortMode,
       editingIndex,
-      currentPage,
-      itemsPerPage,
-      filteredAndSortedItems,
-      totalPages,
-      paginatedItems,
-      showDetails,
-      addEmployee,
-      confirmDelete,
+      showDetailIndex,
+      showAddForm,
       deleteItem,
-      editItem,
+      confirmDelete,
       updateItem,
-      cancelEdit
+      editItem,
+      cancelEdit,
+      showDetails,
+      cancelAdd,
+      // fetch data
+      getCatalogItemData
     };
+  },
+  methods: {
+    convertToBase64Image(item) {
+      console.log(item.image.data);
+      if (!item.image) {
+        return '';
+      }
+      return convertToBase64(item.image.data);
+    }
+  },
+  mounted() {
+    this.getCatalogItemData();
   }
 };
 </script>
@@ -246,5 +235,9 @@ export default {
 }
 .font-weight-bold {
   font-weight: bold;
+}
+.review-image {
+  width: 45px;
+  height: 45px;
 }
 </style>
